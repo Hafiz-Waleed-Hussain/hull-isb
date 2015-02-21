4d7cb903ce96fa156bd094641a07c21177244f67
Ahmad Ali Nasir <ahmad.ali@rockvillegroup.com>
2015-02-21 15:40:34 +0500

    Integrated Parse SDK.

---
+++ android-hull/Civichackathon/Hull/app/src/main/java/com/highndry/hull/ParseApplication.java
@@ -0,0 +1,35 @@
+package com.highndry.hull;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+    private static final String YOUR_APPLICATION_ID = "evTJMDPOKrYSzmbl1n29fdkrf4hxH3auipbVxwIC";
+    private static final String YOUR_CLIENT_KEY = "5kVooJSVArSqhvO9Lv0lBlJ4V5nBZ4UTSMqdo5Ob";
+
+    @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Initialize Crash Reporting.
+    ParseCrashReporting.enable(this);
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY);
+
+
+    ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+    // Optionally enable public read access.
+    // defaultACL.setPublicReadAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+  }
+}
